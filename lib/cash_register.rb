144611eb67a6b4e5569b5b1932b5f4f07f33e389
Flatiron School <flatironschool@Flatirons-MacBook-Air.local>
2017-05-16 19:49:54 -0400

    added initialize and workin on add_item

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,15 @@
+class CashRegister
 
+  attr_accessor :total
+  attr_reader :total, :discount
+
+  def initialize(*discount)
+    @total = 0
+    @discount = discount[0]
+  end
+
+  def add_item(item, price, *quantity)
+    
+  end
+
+end

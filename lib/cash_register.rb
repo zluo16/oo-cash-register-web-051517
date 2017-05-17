1231772dc0c19e3a634c2aa6f084d5bec224ccf1
Flatiron School <flatironschool@Flatirons-Air.lan>
2017-05-16 21:33:32 -0400

    created cash register class

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class CashRegister
 
   attr_accessor :total
@@ -5,11 +7,34 @@ class CashRegister
 
   def initialize(*discount)
     @total = 0
+    @cart = []
     @discount = discount[0]
   end
 
-  def add_item(item, price, *quantity)
-    
+  def add_item(item, price, quantity=1)
+    @total += price * quantity
+
+    while quantity > 0
+      @cart << item
+      quantity -= 1
+    end
+  end
+
+  def apply_discount
+    if self.discount != nil
+      @total -= self.total * self.discount / 100
+      "After the discount, the total comes to $#{@total}."
+    else
+      "There is no discount to apply."
+    end
+  end
+
+  def items
+    @cart
+  end
+
+  def void_last_transaction
+    @total -= self.total
   end
 
 end
